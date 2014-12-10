d8211526c812ded487cecefc070fbd6188415525
Kirito Kun <seotox@gmail.com>
2014-12-10 19:54:34 +0100

    Utility: DisableEmote

---
+++ DisableEmote/Source/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace DisableEmote.Source
+{
+    class Main
+    {
+        public Main()
+        {
+            Game.OnGameProcessPacket += Game_OnGameProcessPacket;
+        }
+
+        void Game_OnGameProcessPacket(GamePacketEventArgs args)
+        {
+            if (args.PacketData[0] == Packet.S2C.PlayEmote.Header)
+            {
+                args.Process = false;
+            }
+        }
+    }
+}
